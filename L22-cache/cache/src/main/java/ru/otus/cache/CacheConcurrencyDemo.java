d446b1848cceac8164927992f8d6a26a04b34421
Galaxy13 <gen.hort54@gmail.com>
2024-10-18 10:40:47 +0300

    2024.10.18 update: linter fix

--- L22-cache/cache/src/main/java/ru/otus/cache/CacheConcurrencyDemo.java
+++ L22-cache/cache/src/main/java/ru/otus/cache/CacheConcurrencyDemo.java
@@ -1,15 +1,16 @@
+package ru.otus.cache;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.otus.cache.HwCache;
-import ru.otus.cache.MyCache;
 
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 // -Xmx256m -Xms256m
-class TestCacheConcurrency {
-    private static final Logger logger = LoggerFactory.getLogger(TestCacheConcurrency.class);
+class CacheConcurrencyDemo {
+    private static final Logger logger = LoggerFactory.getLogger(CacheConcurrencyDemo.class);
+    private static final Random rand = new Random();
 
     public static void main(String[] args) {
         testCacheConcurrency();
@@ -17,7 +18,6 @@ class TestCacheConcurrency {
 
     public static void testCacheConcurrency() {
         HwCache<Integer, Integer> cache = MyCache.create(10_000_000);
-        var rand = new Random();
         cache.addListener((key, value, operation) -> {
             if (operation.equals("gc")) {
                 logger.info("key:{} value:{} ops:{}", key, value, operation);
